f26b21dd975c9e2cd371b3b389e96eeded4a3711
ycx211314@gmail.com <ycx2111314@gmail.com>
2024-07-03 18:08:14 +0800

    ✨ feat: 个人页面开发

--- lib/pages/me/me_page.dart
+++ lib/pages/me/me_page.dart
@@ -11,26 +11,50 @@ class MePage extends StatelessWidget {
   Widget _buildHeader(context) {
     return Container(
       color: Colors.white,
-      padding: EdgeInsets.only(top: 60.h),
-      child: SizedBox(
-        height: 100.h,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(
+      height: 165.h,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Container(
+              margin: EdgeInsets.fromLTRB(0, 0, 0, 25.h),
               height: 64.h,
               child: CircleAvatar(
-                  radius: 50.w,
-                  backgroundImage:
-                      const AssetImage('assets/img/touxiang4.png')),
+                  radius: 64.h,
+                  backgroundColor: Colors.red,
+                  child: ClipOval(
+                    child: Image.asset(
+                      'assets/img/touxiang4.png',
+                      fit: BoxFit.contain, // 使图片适应容器，可能会被裁剪
+                      width: 64.h, // 根据需要调整宽度
+                      height: 64.h, // 根据需要调整高度
+                    ),
+                  ))),
+          Expanded(
+            child: Container(
+              margin: EdgeInsets.fromLTRB(0, 0, 0, 35.h),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Codery",
+                      style: TextStyle(
+                          fontSize: 20.sp, fontWeight: FontWeight.w500),
+                    ),
+                    SizedBox(height: 5.h),
+                    Text(
+                      "这里就是一句话的描述",
+                      style: TextStyle(fontSize: 15.sp, color: Colors.grey),
+                    )
+                  ]),
             ),
-            const Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [Text("Codery"), Text("这里就是一句话的描述"), Text("这里VIP")])
-          ],
-        ),
+          ),
+          const Divider(
+            color: Colors.red,
+            height: 2,
+          )
+        ],
       ),
     );
   }
