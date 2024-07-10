ba36790bdcb9cf56bbc28e8318f798a634a8e7b0
ycx211314@gmail.com <ycx2111314@gmail.com>
2024-07-10 18:12:11 +0800

    ✨ feat: 验证码发送功能

---
+++ lib/common/utils/common_validator.dart
@@ -0,0 +1,36 @@
+String? validateEmail(String? value) {
+  if (value == null || value.isEmpty) {
+    return 'Please enter your email';
+  }
+  // 正则表达式匹配电子邮件格式
+  final RegExp emailRegExp = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+  if (!emailRegExp.hasMatch(value)) {
+    return 'Please enter a valid email';
+  }
+
+  return null;
+}
+
+String? validatePassword(String? value) {
+  if (value == null || value.isEmpty) {
+    return 'Please enter your password';
+  }
+
+  // 检查密码长度
+  if (value.length < 6) {
+    return 'Password must be at least 6 characters';
+  }
+
+  // 检查密码是否包含至少一个大写字母、一个小写字母和一个数字
+  final RegExp uppercaseRegExp = RegExp(r'[A-Z]');
+  final RegExp lowercaseRegExp = RegExp(r'[a-z]');
+  final RegExp digitRegExp = RegExp(r'\d');
+
+  if (!uppercaseRegExp.hasMatch(value) ||
+      !lowercaseRegExp.hasMatch(value) ||
+      !digitRegExp.hasMatch(value)) {
+    return 'Password must contain at least one uppercase letter, one lowercase letter, and one digit';
+  }
+
+  return null;
+}
--- lib/pages/sign_in/forget_password/forget_password_page.dart
+++ lib/pages/sign_in/forget_password/forget_password_page.dart
@@ -1,9 +1,176 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:codery/common/icons/iconfont.dart';
+import 'package:codery/common/utils/color_helper.dart';
+import 'package:codery/common/utils/common_validator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:otp_text_field/otp_field.dart';
+import 'package:otp_text_field/style.dart';
 
 @RoutePage()
-class ForgetPasswordPage extends StatelessWidget {
+class ForgetPasswordPage extends StatefulWidget {
   const ForgetPasswordPage({super.key});
+
+  @override
+  State<ForgetPasswordPage> createState() => _ForgetPasswordPageState();
+}
+
+class _ForgetPasswordPageState extends State<ForgetPasswordPage> {
+  int _currentIndex = 0;
+
+  final TextEditingController _emailController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  final FocusNode _focusNodeEmaiL = FocusNode();
+  Color _emailIconColor = ColorHelper.hexToColor("#9098B1");
+
+  @override
+  void initState() {
+    _focusNodeEmaiL.addListener(() {
+      setState(() {
+        _emailIconColor = _focusNodeEmaiL.hasFocus
+            ? ColorHelper.hexToColor("#40BFFF")
+            : ColorHelper.hexToColor("#9098B1");
+      });
+    });
+    super.initState();
+  }
+
+//填写邮件页面
+  _buildEmailContainer(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(16.w),
+      color: Colors.white,
+      child: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Your registed Email",
+              style: TextStyle(
+                fontSize: 14.sp,
+                color: ColorHelper.hexToColor("#223263"),
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            SizedBox(
+              height: 6.h,
+            ),
+            Container(
+              // height: 48.h,
+              color: Colors.white,
+              child: TextFormField(
+                controller: _emailController,
+                focusNode: _focusNodeEmaiL,
+                validator: validateEmail,
+                style: TextStyle(
+                    fontSize: 12.sp, color: ColorHelper.hexToColor("#9098B1")),
+                decoration: InputDecoration(
+                  hintText: "Enter your email",
+                  helperText: "the email address to retrieve your password",
+                  hintStyle: TextStyle(
+                      color: ColorHelper.hexToColor("#9098B1"),
+                      fontSize: 12.sp),
+                  prefixIcon: Icon(
+                    Iconfont.email,
+                    size: 18.sp,
+                    color: _emailIconColor,
+                  ),
+                  border: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: ColorHelper.hexToColor("#EBF0FF"))),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        color: ColorHelper.hexToColor("#40BFFF")), // 启用状态下的边框颜色
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        color: ColorHelper.hexToColor("#40BFFF")), // 获得焦点时的边框颜色
+                  ),
+                  errorStyle: const TextStyle(height: 0),
+                  contentPadding: EdgeInsets.symmetric(vertical: 16.h),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+//填写验证码页面
+  _buildValidatorCodeContainer(BuildContext context) {
+    return Container(
+        margin: EdgeInsets.all(16.h),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                "Verification Code",
+                style: TextStyle(
+                  fontSize: 16.sp,
+                  color: ColorHelper.hexToColor("#333333"),
+                ),
+              ),
+              SizedBox(height: 18.h),
+              Container(
+                child: OTPTextField(
+                  length: 4,
+                  width: 400.w,
+                  fieldWidth: 50,
+                  style: const TextStyle(fontSize: 17),
+                  textFieldAlignment: MainAxisAlignment.spaceAround,
+                  fieldStyle: FieldStyle.box,
+                  onCompleted: (pin) {
+                    print("Completed: $pin");
+                  },
+                ),
+              ),
+              SizedBox(
+                height: 20.h,
+              ),
+              Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Didn't receive the code?",
+                      style: TextStyle(
+                        fontSize: 12.sp,
+                        color: ColorHelper.hexToColor("#333333"),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        print("resend");
+                      },
+                      child: Text(
+                        "resend",
+                        style: TextStyle(
+                            color: ColorHelper.hexToColor("#40BFFF"),
+                            fontSize: 12.sp,
+                            fontWeight: FontWeight.w600),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(margin: EdgeInsets.only(top: 10.h))
+            ]));
+  }
+
+  _handle_next_action(BuildContext context) {
+    if (_currentIndex == 0) {
+      //第一步验证邮箱
+      if (_formKey.currentState?.validate() ?? false) {
+        //TODO 验证邮箱是否存在
+        setState(() {
+          _currentIndex = 1;
+        });
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,11 +179,19 @@ class ForgetPasswordPage extends StatelessWidget {
         title: const Text('Retrieve password'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'ForgetPasswordPage is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      backgroundColor: Colors.white,
+      body: IndexedStack(
+        index: _currentIndex,
+        children: [
+          _buildEmailContainer(context),
+          _buildValidatorCodeContainer(context)
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _handle_next_action(context);
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
